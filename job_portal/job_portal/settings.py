e3364737192bee552d2b317e50c65d2d57cf15f9
Devaswin <ksdevaswin@gmail.com>
2025-09-22 16:21:40 +0530

    added job posting

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -148,13 +148,7 @@ SIMPLE_JWT ={
 
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-#EMAIL_HOST = "smtp.gmail.com"
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = "devaswinksukumaran@gmail.com"           # replace with your Gmail
-#EMAIL_HOST_PASSWORD = "zqrk dzus uugb uozk"         # generate app password in Google
-#DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 
 
